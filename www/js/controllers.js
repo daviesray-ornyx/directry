eb2d53a97a734b3afbf6b6c198e93fa3759441e6
daviesray-ornyx <daviesray.ornyx@gmail.com>
2015-07-07 00:26:05 +0300

    Defining server and client settings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,15 +55,7 @@ angular.module('directory.controllers', ['directory.services'])
     $scope.search();
   }
 
-  $scope.fullProfileImagePath = function(name){
-    if(!name){
-      //No image, use a default image
-      return "./img/business_placeholder.jpg";
-    }
-    else{
-      return "http://localhost:9804/uploads/images/profile_pics/" + name;
-    }
-  }
+  $scope.fullProfileImagePath = $rootScope.fullProfileImagePath(name);
 
   $scope.showDetail = function(contact){
     console.log(contact);
@@ -156,15 +148,7 @@ angular.module('directory.controllers', ['directory.services'])
     $scope.search();
   }
 
-  $scope.fullProfileImagePath = function(name){
-    if(!name){
-      //No image, use a default image
-      return "./img/business_placeholder.jpg";
-    }
-    else{
-      return "http://localhost:9804/uploads/images/profile_pics/" + name;
-    }
-  }
+  $scope.fullProfileImagePath = $rootScope.fullProfileImagePath(name);
 
   $scope.showDetail = function(contact){
     console.log(contact);
@@ -257,15 +241,7 @@ angular.module('directory.controllers', ['directory.services'])
     $scope.search();
   }
 
-  $scope.fullProfileImagePath = function(name){
-    if(!name){
-      //No image, use a default image
-      return "./img/business_placeholder.jpg";
-    }
-    else{
-      return "http://localhost:9804/uploads/images/profile_pics/" + name;
-    }
-  }
+  $scope.fullProfileImagePath = $rootScope.fullProfileImagePath(name);
 
   $scope.showDetail = function(contact){
     console.log(contact);
