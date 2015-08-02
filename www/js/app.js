516ef299a66b600f68ccda2aff05528bd3299951
daviesray-ornyx <daviesray.ornyx@gmail.com>
2015-08-02 16:36:27 +0300

    New Admob

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('directry', ['ionic', 'directory.controllers', 'directory.services'])
+angular.module('directry', ['ionic', 'directory.controllers', 'directory.services', ])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -14,6 +14,29 @@ angular.module('directry', ['ionic', 'directory.controllers', 'directory.service
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    if(window.plugins && window.plugins.AdMob) {
+            var admob_key = device.platform == "Android" ? "ca-app-pub-6699142760491850/5045443529" : "ca-app-pub-6699142760491850/5045443529";
+            var admob = window.plugins.AdMob;
+            admob.createBannerView(
+                {
+                    'publisherId': admob_key,
+                    'adSize': admob.AD_SIZE.BANNER,
+                    'bannerAtTop': false
+                },
+                function() {
+                    admob.requestAd(
+                        { 'isTesting': false }, 
+                        function() {
+                            admob.showAd(true);
+                        },
+                        function() { console.log('failed to request ad'); }
+                    );
+                },
+                function() { console.log('failed to create banner view'); }
+            );
+        }
+
   });
 
 })
