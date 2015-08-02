c6fc393e915ce92e1bd3ffaa214dccbb7e5e4673
daviesray-ornyx <daviesray.ornyx@gmail.com>
2015-08-02 14:15:07 +0300

    Tring

--- www/js/app.js
+++ www/js/app.js
@@ -15,84 +15,73 @@ angular.module('directry', ['ionic', 'directory.controllers', 'directory.service
       StatusBar.styleDefault();
     }
 
-    if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
-        document.addEventListener('deviceready', initApp, false);
-    } else {
-        initApp();
-    }
+    function onLoad() {
+            if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+                document.addEventListener('deviceready', initApp, false);
+            } else {
+                initApp();
+            }
+        }
+    	var admobid = {};
+    	if( /(android)/i.test(navigator.userAgent) ) {
+    		admobid = { // for Android
+    			banner: 'ca-app-pub-6699142760491850/5045443529',
+    			interstitial: 'ca-app-pub-6699142760491850/8733293122'
+    		};
+    	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    		admobid = { // for iOS
+    			banner: 'ca-app-pub-6869992474017983/4806197152',
+    			interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    		};
+    	} else {
+    		admobid = { // for Windows Phone
+    			banner: 'ca-app-pub-6699142760491850/5045443529',
+    			interstitial: 'ca-app-pub-6699142760491850/8733293122'
+    		};
+    	}
 
-	var admobid = {};
-  console.log("Testing for admobid");
-	if( /(android)/i.test(navigator.userAgent) ) {
-		admobid = { // for Android
-			banner: 'ca-app-pub-6699142760491850/5045443529',
-			interstitial: 'ca-app-pub-6699142760491850/8733293122'
-		};
-	}
-  else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-		admobid = { // for iOS
-			banner: 'ca-app-pub-6869992474017983/4806197152',
-			interstitial: 'ca-app-pub-6869992474017983/7563979554'
-		};
-	}
-  else {
-		admobid = { // for Windows Phone
-      banner: 'ca-app-pub-6699142760491850/5045443529',
-			interstitial: 'ca-app-pub-6699142760491850/8733293122'
-		};
-	}
+        function initApp() {
+    		if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+    		initAd();
+            // display the banner at startup
+            createSelectedBanner();
+        }
+        function initAd(){
+            var defaultOptions = {
+                // adSize: 'SMART_BANNER',
+                // width: integer, // valid when set adSize 'CUSTOM'
+                // height: integer, // valid when set adSize 'CUSTOM'
+                position: AdMob.AD_POSITION.BOTTOM_CENTER,
+                // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
+                bgColor: 'black', // color name, or '#RRGGBB'
+                // x: integer,		// valid when set position to 0 / POS_XY
+                // y: integer,		// valid when set position to 0 / POS_XY
+                isTesting: true, // set to true, to receiving test ad for testing purpose
+                // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
+            };
+            AdMob.setOptions( defaultOptions );
+            registerAdEvents();
+        }
+        // optional, in case respond to events or handle error
+        function registerAdEvents() {
+            // new events, with variable to differentiate: adNetwork, adType, adEvent
+            document.addEventListener('onAdFailLoad', function(data){
+            	alert('error: ' + data.error +
+            			', reason: ' + data.reason +
+            			', adNetwork:' + data.adNetwork +
+            			', adType:' + data.adType +
+            			', adEvent:' + data.adEvent); // adType: 'banner', 'interstitial', etc.
+            });
+            document.addEventListener('onAdLoaded', function(data){});
+            document.addEventListener('onAdPresent', function(data){});
+            document.addEventListener('onAdLeaveApp', function(data){});
+            document.addEventListener('onAdDismiss', function(data){});
+        }
+        // preppare and load ad resource in background, e.g. at begining of game level
+      if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
 
-  console.log(admobid);
-
-    function initApp() {
-		  if (! window.AdMob ) { alert( 'admob plugin not ready' ); return; }
-  		initAd();
-      // display the banner at startup
-      createSelectedBanner();
-    }
-    function initAd(){
-        var defaultOptions = {
-            // adSize: 'SMART_BANNER',
-            // width: integer, // valid when set adSize 'CUSTOM'
-            // height: integer, // valid when set adSize 'CUSTOM'
-            position: AdMob.AD_POSITION.BOTTOM_CENTER,
-            // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
-            bgColor: 'black', // color name, or '#RRGGBB'
-            // x: integer,		// valid when set position to 0 / POS_XY
-            // y: integer,		// valid when set position to 0 / POS_XY
-            isTesting: false, // set to true, to receiving test ad for testing purpose
-            // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
-        };
-        AdMob.setOptions( defaultOptions );
-        registerAdEvents();
-    }
-    // optional, in case respond to events or handle error
-    function registerAdEvents() {
-        // new events, with variable to differentiate: adNetwork, adType, adEvent
-        document.addEventListener('onAdFailLoad', function(data){
-        	alert('error: ' + data.error +
-        			', reason: ' + data.reason +
-        			', adNetwork:' + data.adNetwork +
-        			', adType:' + data.adType +
-        			', adEvent:' + data.adEvent); // adType: 'banner', 'interstitial', etc.
-        });
-        document.addEventListener('onAdLoaded', function(data){});
-        document.addEventListener('onAdPresent', function(data){});
-        document.addEventListener('onAdLeaveApp', function(data){});
-        document.addEventListener('onAdDismiss', function(data){});
-    }
-
-    if(window.AdMob) window.AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:true} );
-    // show the interstitial later, e.g. at end of game level
-    if(window.AdMob) AdMob.showInterstitial();
-
-    // it will display smart banner at top center, using the default options
-    if(window.AdMob) window.AdMob.createBanner( {
-        adId: admobid.banner,
-        position: AdMob.AD_POSITION.TOP_CENTER,
-        autoShow: true
-      }
-    );
+      // show the interstitial later, e.g. at end of game level
+      if(AdMob) AdMob.showInterstitial();
   })
 })
 
